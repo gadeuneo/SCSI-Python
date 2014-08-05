9baaf08fe36ac4edcc5cd6d11329cd3104788dde
gadeuneo <gadeuneo@users.noreply.github.com>
2014-08-05 09:46:23 -0500

    bike.py
    calculates trip time

---
+++ bike.py
@@ -0,0 +1,12 @@
+#James Gardner
+
+tripDist = 226 # total miles in the trip
+bikeSpeed = 15 # typcial bicycling speed miles per hour
+
+tripTime = tripDist / bikeSpeed # in hours
+tripHours = int(tripTime)
+tripMins = int ( round( (tripTime - tripHours) * 60) )
+print
+print "To bike", tripDist, "miles, at",
+print bikeSpeed, "miles per hour,",
+print "takes", tripHours, "hours and", tripMins, "minutes"
