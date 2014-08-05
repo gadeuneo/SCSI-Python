9b520f98dbbc9d2b6ad1cd55cd84dd87bda842dc
gadeuneo <gadeuneo@users.noreply.github.com>
2014-08-05 09:48:51 -0500

    flower.py
    calculates number of flowers

---
+++ flower.py
@@ -0,0 +1,16 @@
+# James Gardner
+
+bedLen = 2 # in feet
+bedWid = 1.5 # in feet
+
+bedLenInch = bedLen * 12 # bedLen in inches
+bedWidInch = bedWid * 12 # bedWid in inches
+
+totalArea = bedLenInch * bedWidInch
+
+flowerArea = 6 * 6 #in inches
+
+flowerNum = totalArea / flowerArea
+
+print
+print flowerNum
