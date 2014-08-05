029e5366c168760ed0871ec44914ca38e3bba3d0
gadeuneo <gadeuneo@users.noreply.github.com>
2014-08-05 11:03:08 -0500

    booleanEx.py
    boolean code example

---
+++ booleanEx.py
@@ -0,0 +1,14 @@
+#James Gardner
+
+print (35 and 49)
+print (35 and 0)
+print (35 or 49)
+print (35 or 0)
+if 293:
+    print "option a"
+else:
+    print "option b"
+if []:
+    print "option a"
+else:
+    print "option b"
