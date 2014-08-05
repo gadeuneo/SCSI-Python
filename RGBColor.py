551b6e8109ee247e8e0fece985b147230ad1c631
gadeuneo <gadeuneo@users.noreply.github.com>
2014-08-05 11:09:20 -0500

    RGBColor.py
    Red Green Blue Color python test

---
+++ RGBColor.py
@@ -0,0 +1,18 @@
+#James Gardner
+
+import cv2
+import numpy
+image = cv2.imread("TestImages/shops.jpg")
+(bc, gc, rc) = cv2.split(image)
+cv2.imshow("Blue channel", bc)
+cv2.imshow("Green channel", gc)
+cv2.imshow("Red channel", rc)
+cv2.waitKey(0)
+zc = numpy.zeros( bc.shape, numpy.uint8 )
+blueImg = cv2.merge((bc, zc, zc))
+cv2.imshow("Blue channel", blueImg)
+greenImg = cv2.merge((zc, gc, zc))
+cv2.imshow("Green channel", greenImg)
+redImg = cv2.merge((zc, zc, rc))
+cv2.imshow("Red channel", redImg)
+cv2.waitKey(0)
