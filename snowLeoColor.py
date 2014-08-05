6bba13f5ed129ff9e8ce3732312998fa9985c878
gadeuneo <gadeuneo@users.noreply.github.com>
2014-08-05 10:25:07 -0500

    Update snowLeoColor.py
    creates blank image after key hit

--- snowLeoColor.py
+++ snowLeoColor.py
@@ -8,5 +8,6 @@ img1 = cv2.imread("TestImages/SnowLeo2.jpg")
 cv2.imshow("SnowLeo2.jpg" , img1)
 cv2.waitKey(0)
 blankImg = numpy.zeros((500, 500, 3), numpy.uint8)
-cv2.imshow("SnowLeo2.jpg",blankImg)
-
+cv2.imshow("SnowLeo2.jpg", blankImg)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
