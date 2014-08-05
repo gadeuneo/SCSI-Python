ea32b7f50132907220baf82f934adf79e8855fcd
gadeuneo <gadeuneo@users.noreply.github.com>
2014-08-05 09:51:13 -0500

    cv2test3.py
    working on it

---
+++ cv2test3.py
@@ -0,0 +1,10 @@
+# James Gardner
+
+import cv2
+
+lst1 = ["TestImages/SnowLeo1.jpg", "TestImages/SnowLeo2.jpg", "TestImages/Puzzle1.jpg", "TestImages/Coins1.jpg", "TestImages/Coins2.jpg", "TestImages/DollarCoin.jpg", "TestImages/frankenstein.jpg", "TestImages/gorge.jpg", "TestImages/ghostrider.jpg", "TestImages/Puzzle2.jpg"]
+
+for var in lst1:
+    cv2.imread("var")
+    cv2.waitKey(0)
+cv2.destroyAllWindows()
