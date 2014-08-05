0e05c671e05a6051005af5301640b14aacacf4d6
gadeuneo <gadeuneo@users.noreply.github.com>
2014-08-05 10:19:32 -0500

    snowLeoColor.py
    outlining parts of snow leo

---
+++ snowLeoColor.py
@@ -0,0 +1,12 @@
+# James Gardner
+
+
+import numpy
+import cv2
+
+img1 = cv2.imread("TestImages/SnowLeo2.jpg")
+cv2.imshow("SnowLeo2.jpg" , img1)
+cv2.waitKey(0)
+blankImg = numpy.zeros((500, 500, 3), numpy.uint8)
+cv2.imshow("SnowLeo2.jpg",blankImg)
+
