2a40f7bf1b182ed7c04f46ddb27bd6e5c0b75e8b
gadeuneo <gadeuneo@users.noreply.github.com>
2014-08-05 10:39:58 -0500

    Update if_else.py
    if else

--- if_else.py
+++ if_else.py
@@ -4,22 +4,40 @@ import cv2
 
 
 img1 = cv2.imread("TestImages/SnowLeo1.jpg")
+if img1.any():
+    cv2.imread == None
+else:
+    cv2.imread != None
+    cv2.waitKey(0)
 cv2.imshow("Pic 1", img1)
 cv2.waitKey(0)
 
+
 img2 = cv2.imread("TestImages/SnowLeo2.jpg")
-
+if img2.any():
+    cv2.imread == None
+else:
+    cv2.imread != None
+    cv2.waitKey(0)
 cv2.imshow("Pic 1", img2)
 cv2.waitKey(0)
 
-img2 = cv2.imread("TestImages/Puzzle1.jpg")
-
-cv2.imshow("Pic 1", img2)
+img3 = cv2.imread("TestImages/Puzzle1.jpg")
+if img3.any():
+    cv2.imread == None
+else:
+    cv2.imread != None
+    cv2.waitKey(0)
+cv2.imshow("Pic 1", img3)
 cv2.waitKey(0)
 
-img2 = cv2.imread("TestImages/Coins1.jpg")
-
-cv2.imshow("Pic 1", img2)
+img4 = cv2.imread("TestImages/Coins1.jpg")
+if img4.any():
+    cv2.imread == None
+else:
+    cv2.imread != None
+    cv2.waitKey(0)
+cv2.imshow("Pic 1", img4)
 cv2.waitKey(0)
 
 cv2.destroyAllWindows()
