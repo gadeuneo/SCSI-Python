92beef2e5f47cfb352edd926f9d2b07e2289b30f
gadeuneo <gadeuneo@users.noreply.github.com>
2014-08-05 10:58:14 -0500

    whileLoopEx.py
    example while loop python doesn't work????

---
+++ whileLoopEx.py
@@ -0,0 +1,14 @@
+# James Gardner
+
+
+
+
+total = 0
+numbers = [35, -2, 27, 70, 12]
+i = 0
+while total < 100:
+    total = total + numbers[i]
+    print “Total = ”, total
+    i = i + 1
+print “ -----------------------”
+print “Final total =”, total
