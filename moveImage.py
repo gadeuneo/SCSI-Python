7d6d74e5ee38bf7ba39dc174b59367f9f1e541d2
gadeuneo <gadeuneo@users.noreply.github.com>
2014-08-06 11:22:00 -0500

    moves images successfully!!!!

--- moveImage.py
+++ moveImage.py
@@ -8,30 +8,29 @@ img = cv2.imread("TestImages/SnowLeo2.jpg")
 cv2.imshow("Original", img)
 (rows, cols, depth) = img.shape
 
-cv2.waitKey(0)
+
+while (True):
+
+    code = cv2.waitKey(0)
+    char = chr(code & 0xFF)
+    
+    if char == 'a':
+        transMatrix = numpy.float32([[1,0,-30], [0,1,0]])
+        transImag = cv2.warpAffine(img, transMatrix, (cols, rows))
+        cv2.imshow("Translated", transImag)
+    elif char == 'w':
+        transMatrix = numpy.float32([[1,0,0], [0,1,-30]])
+        transImag = cv2.warpAffine(img,transMatrix, (cols,rows))
+        cv2.imshow("Translated", transImag)
+    elif char == 'd':
+        transMatrix = numpy.float32([[1,0,30], [0,1,0]])
+        transImag = cv2.warpAffine(img, transMatrix, (cols,rows))
+        cv2.imshow("Translated", transImag)
+    elif char == 's':
+        transMatrix = numpy.float32([[1,0,0], [0,1,30]])
+        transImag = cv2.warpAffine(img,transMatrix, (cols,rows))
+        cv2.imshow("Translated", transImag)
+    elif char == 'q':
+        break
+    img = transImag
 cv2.destroyAllWindows()
-cv2.waitKey(0)
-
-code = cv2.waitKey(0)
-char = chr(code & 0xFF)
-
-if char == 'a':
-    transMatirx = numpy.float32([[1,0,30], [0,1,0]])
-    transImag = cv2.warpAffine(img, transMatrix, (cols, rows))
-    cv2.imshow("Translated", transImag)
-elif char == 'w':
-    transMatrix = numpy.float32([[1,0,0], [0,1,30]])
-    transImag = cv2.warpAffine(img,transMatrix, (cols,rows))
-    cv2.imshow("Translated", transImag)
-elif char == 'd':
-    transMatrix = numpy.float32([[1,0,-30], [0,1,0]])
-    transImag = cv2.warpAffine(img, transMatrix, (cols,rows))
-    cv2.imshow("Translated", transImag)
-elif char == 's':
-    transMatirx = numpy.float32([[1,0,0], [0,1,-30]])
-    transImag = cv2.warpAffine(img,transMatrix, (cols,rows))
-    cv2.imshow("Translated", transImag)
-
-transMatrix = numpy.float32([[1, 0, 30], [0, 1, 50]])
-transImag = cv2.warpAffine(img, transMatrix, (cols, rows))
-cv2.imshow("Translated", transImag)
