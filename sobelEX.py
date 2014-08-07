ef70635f5d64c7a97a02fc7eee66886313d33332
gadeuneo <gadeuneo@users.noreply.github.com>
2014-08-07 09:20:16 -0500

    sobel-gradient exapmle

---
+++ sobelEX.py
@@ -0,0 +1,24 @@
+#James Gardner
+
+import cv2
+import numpy
+
+img = cv2.imread("TestImages/shops.jpg")
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#Compute gradient in horizontal direction (detects vertical edges)
+sobleValsHorz = cv2.Sobel(gray, cv2.CV_32F, 1, 0)
+horzImg = cv2.convertScaleAbs(sobelValsHorz)
+cv2.imshow("horizontal gradient", horzImg)
+
+#Computer gradient in vertical direction (Detects horizontal edges)
+sobelValsVerts = cv2.Sobel(gray, cv2.CV_32F, 0, 1)
+vertImg = cv2.convertScaleAbs(sobelValsVerts)
+cv2.imshow("vertical gradient", vertImg)
+
+#Combine the two gradients
+sobelComb = cv2.addWeighted(sobelValsHorz, 0.5,
+                            sobelValsHerts, 0.5, 0)
+#Convert back to uint8
+sobelImg = cv2.convertScaleAbs(sobelComb)
+cv2.imshow("Sobel", sobelImg)
