d7dc287ede266728bd986ad385f95e06dfe0d67d
gadeuneo <gadeuneo@users.noreply.github.com>
2014-08-07 09:09:10 -0500

    added destroy all windows

--- grayHist.py
+++ grayHist.py
@@ -5,5 +5,7 @@ import numpy
 
 img = cv2.imread("TestImages/shops.jpg")
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-hist = cv2.calcHist([gray], [0], None, [256], [0, 256])
+hist = cv2.calcHist([gray], [0], None, [8], [0, 8])
 cv2.imshow("img",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
