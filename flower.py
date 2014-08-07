194e296ffd06dcd50e64206fc125e9b04641a6d4
gadeuneo <gadeuneo@users.noreply.github.com>
2014-08-07 10:56:48 -0500

    added easier var value chanes

--- flower.py
+++ flower.py
@@ -2,13 +2,15 @@
 
 bedLen = 2 # in feet
 bedWid = 1.5 # in feet
+flowerWid = 6 # in inches
+flowerLen = 6 #in inches
 
 bedLenInch = bedLen * 12 # bedLen in inches
 bedWidInch = bedWid * 12 # bedWid in inches
 
-totalArea = bedLenInch * bedWidInch
+totalArea = bedLenInch * bedWidInch #total area in square inches
 
-flowerArea = 6 * 6 #in inches
+flowerArea = flowerWid * flowerLen #in inches
 
 flowerNum = totalArea / flowerArea
 
