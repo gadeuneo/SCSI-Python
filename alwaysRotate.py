fa69b72a891f8bfa7e5ef1218d712157c1aa9a86
gadeuneo <gadeuneo@users.noreply.github.com>
2014-08-07 10:03:54 -0500

    added key hit and exit key

--- alwaysRotate.py
+++ alwaysRotate.py
@@ -3,17 +3,26 @@
 import cv2
 import numpy
 
-speed = 10
+speed = 2
 
 img = cv2.imread("TestImages/SnowLeo2.jpg")
+cv2.imshow("Original", img)
+
 
 #cv2.imshow("Original", img)
 
 (rows, cols, depth) = img.shape
 
 while (True):
-    speed += 10
-    rotMat = cv2.getRotationMatrix2D( (cols / 2, rows / 3), speed, 1)
-    rotImg = cv2.warpAffine(img, rotMat, (cols,rows))
-    cv2.imshow("Rotated", rotImg)
-    cv2.waitKey(0)
+    code = cv2.waitKey(0)
+    char = chr(code & 0xFF)   
+    
+    if char == 'r':
+        speed += 5
+        rotMat = cv2.getRotationMatrix2D( (cols / 2, rows / 3), speed, 1)
+        rotImg = cv2.warpAffine(img, rotMat, (cols,rows))
+        cv2.imshow("Rotated", rotImg)
+        cv2.waitKey(1)
+    elif char == 'q':
+        break
+cv2.destroyAllWindows()
